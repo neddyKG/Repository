c08745b69a207680673459c9b988477c00540127
neddyKG <gonzalezneddy@gmail.com>
2018-08-11 12:06:34 -0400

    create repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
